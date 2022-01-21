43408f54f480d8662a274bb2432b51b2e081e149
yee23456 <b10805025@ctbc.edu.tw>
2022-01-21 15:10:08 +0800

    2022/1/21

--- 11.py
+++ 11.py
@@ -5,16 +5,16 @@ Created on Thu Jan 20 19:15:32 2022
 @author: 沈睿朋
 """
 #%%
-def maxArea(height):
-        number = []
-        for i in range(len(height)):
-            for j in range(len(height)):
-                if height[i] < height[j]:
-                    number.append(height[i]*abs(i-j))
-                else:
-                    number.append(height[j]*abs(i-j))
-        return print(max(number))
-maxArea([1,1])
+# def maxArea(height):
+#         number = []
+#         for i in range(len(height)):
+#             for j in range(len(height)):
+#                 if height[i] < height[j]:
+#                     number.append(height[i]*abs(i-j))
+#                 else:
+#                     number.append(height[j]*abs(i-j))
+#         return print(max(number))
+# maxArea([1,1])
 #%%
 class Solution:
     def maxArea(self, height):
