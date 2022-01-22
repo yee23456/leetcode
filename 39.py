3902897ec36d0a4d4898a8b94f7dde949fd6df51
yee23456 <b10805025@ctbc.edu.tw>
2022-01-22 15:34:18 +0800

    2022/1/22

--- 39.py
+++ 39.py
@@ -17,6 +17,34 @@ for i in range(len(a)):
     b = [a[j:i+1] for j in range(i+1)]
     print(b)
 #%%
+# def dp(candidates,target,number =[],target_sum = 0):
+#     candidates.sort()
+#     for i in range(len(candidates)):
+#         if target_sum <7:
+#%% DP  
+def combinationSum(candidates,target):
+    answer = []
+
+    universe = [([], 0)]
+    for n in candidates:
+        for (ls, v) in universe:
+            if v + n == target:
+                answer.append(ls + [n])
+            elif v + n < target:
+                universe.append((ls + [n], v + n))
+    return print(answer)
+combinationSum([2,3,5],7)
+
+
+
+
+
+
+
+
+
+
+#%%
 # candidates = [2,3,6,7]
 # target = 7
 # result = []
@@ -34,17 +62,17 @@ for i in range(len(a)):
 # print(result)
             
 #%% 深度優先搜尋 DFS 上網查到別人寫出來的
-class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        result = []
-        candidates.sort()
-        def dfs(candidates, target, path):
-            if target == 0:
-                result.append(path)
-            elif not candidates or target < candidates[0]:
-                return
-            else:
-                for i,c in enumerate(candidates):
-                    dfs(candidates[i:], target -c, path+[c])
-        dfs(candidates, target, [])
-        return result
+# class Solution:
+#     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+#         result = []
+#         candidates.sort()
+#         def dfs(candidates, target, path):
+#             if target == 0:
+#                 result.append(path)
+#             elif not candidates or target < candidates[0]:
+#                 return
+#             else:
+#                 for i,c in enumerate(candidates):
+#                     dfs(candidates[i:], target -c, path+[c])
+#         dfs(candidates, target, [])
+#         return result
